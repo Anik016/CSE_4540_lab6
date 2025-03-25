80287476693e7a153d1899948995828e5150e9b9
Anik016 <127922585+Anik016@users.noreply.github.com>
2025-03-25 17:12:18 +0600

    Updatd

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  pass: String,
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  pass: { type: String, required: true }
 });
 
 module.exports = mongoose.model('User', userSchema);
